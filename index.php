4a8f422cd0fce2fec3e21a33edc9e34c89e61c09
qwertygehaktbal <s1145629@student.windesheim.nl>
2019-09-09 15:25:23 +0200

    website template toegevoegd

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Test </title>
+    <meta http-equiv="refresh" content="5">
+    <link rel="stylesheet" href="main2.css" type="text/css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <p>Job's keukenkastje</p>
+    <button type="button" class="btn-lg btn-block btn-danger">thuis</button>
+    <br>
+    <br>
+    <table class="table table-striped table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Tijd</th>
+            </tr>
+        </thead>
+        <tr>
+            <td>14:02</td>
+            <td>20-4-2018</td>
+        </tr>
+        <tr>
+
+        </tr>
+        <tr>
+
+        </tr>
+    </table>
+
+    <?php
+/**
+ * Created by PhpStorm.
+ * User: Daan
+ * Date: 9-9-2019
+ * Time: 12:53
+ */
+?>
+</body>
+</html>
