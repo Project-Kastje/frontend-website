2824eb9280ff29e44e91065ab4078f8d1f404192
DerpLvlAsian <55085257+DerpLvlAsian@users.noreply.github.com>
2019-09-10 12:28:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,35 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> Test </title>
+    <title> Job's Keukenkastje </title>
     <meta http-equiv="refresh" content="5">
     <link rel="stylesheet" href="main2.css" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-    <p>Job's keukenkastje</p>
-    <button type="button" class="btn-lg btn-block btn-danger">thuis</button>
+	<div class="header">
+		<p>Job's keukenkastje</p>
+    </div>
+	<div class="headerspace"></div>
+	<div class="button">
+		<button type="button" class="btn-lg btn-block btn-danger">
+			<p class="buttontext">Thuis</p>
+		</button>
+    </div>
+	<br>
     <br>
-    <br>
-    <table class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Tijd</th>
-            </tr>
-        </thead>
-        <tr>
-            <td>14:02</td>
-            <td>20-4-2018</td>
-        </tr>
-        <tr>
+    <div class="table">
+		<table class="table table-striped table-bordered">
+			<thead>
+				<tr>
+					<th>Tijd</th>
+					<th>Datum</th>
+				</tr>
+			</thead>
+			<tr>
+				<td>14:02</td>
+				<td>20-4-2018</td>
+			</tr>
+			<tr>
+	
+			</tr>
+			<tr>
 
-        </tr>
-        <tr>
-
-        </tr>
-    </table>
-
+			</tr>
+		</table>
+	</div>
     <?php
 /**
  * Created by PhpStorm.
@@ -38,5 +46,7 @@
  * Time: 12:53
  */
 ?>
+	<div class="testspace">
+	</div>
 </body>
 </html>
