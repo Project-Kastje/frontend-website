badca036a7e14234b14385bf2fcce0d793c09cbd
JobVulkers <55084678+JobVulkers@users.noreply.github.com>
2019-09-11 14:43:39 +0200

    WERKT

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title> Job's Keukenkastje </title>
-    <meta http-equiv="refresh" content="10000">
+    !--<meta http-equiv="refresh" content="1000">
     <link rel="stylesheet" href="main2.css" type="text/css">
     <link rel="stylesheet" href="bootstrap.min.css">
 </head>
@@ -16,23 +16,29 @@
 			<p class="buttontext">Thuis</p>
 		</button>
     </div>
-    <?php
-    $servername = "localhost";
-    $username = "backend-service";
-    $password = "geheim!";
-    $db = "kastje";
-    try {
-       $conn = new PDO("mysql:host=$servername;dbname=$db, $username, $password, $db);
-       // set the PDO error mode to exception
-       $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-       echo "Connected successfully";
-       }
-    catch(PDOException $e)
-       {
-       echo "Connection failed: " . $e->getMessage();
-       }
-    ?>
-</table>
+	<br>
+  <?php
+  $dbname = 'kastje';
+  $con = mysqli_connect('localhost', 'backend-service','geheim!', 'kastje');
+  if (!$con) {
+      echo 'Could not connect to mysql';
+      exit;
+  }
+
+  $sql = "SELECT * FROM history WHERE 1";
+  $result = mysqli_query($con,$sql);
+
+  if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysqli_error();
+    exit;
+}
+
+while ($row = mysqli_fetch_row($result)) {
+    echo "ID: {$row[0]}\n";
+    echo "Tijd: {$row[1]}\n";
+}
+  ?>
 
 </body>
 </html>
