58127312feba18a061b865e6716437c18508e2fd
DerpLvlAsian <ronaldbijsma@gmail.com>
2019-09-13 12:48:08 +0200

    yeet

--- index.php
+++ index.php
@@ -5,20 +5,6 @@
     <meta http-equiv="refresh" content="4">
     <link rel="stylesheet" href="main2.css" type="text/css">
     </style>
-    <script>
-	var alarm = 0;
-	function button() {
-		if (alarm == 1) {
-			alarm = 0;
-			document.getElementById("buttontext").innerHTML = "Thuis";
-			document.getElementById("thuis").style.backgroundColor = "green";
-		} else {
-			alarm = 1;
-			document.getElementById("buttontext").innerHTML = "Weg";
-			document.getElementById("thuis").style.backgroundColor = "red";
-		}
-	}
-    </script>
 </head>
 <body>
 	<div class="header">
@@ -30,15 +16,6 @@
 			<p id="buttontext">Thuis</p>
 		</button>
   </div>
-  <script>
-	if (alarm == 0) {
-		document.getElementById("buttontext").innerHTML = "Thuis";
-		document.getElementById("thuis").style.backgroundColor = "green";
-	} else {
-		document.getElementById("buttontext").innerHTML = "Weg";
-		document.getElementById("thuis").style.backgroundColor = "red";
-	}
-    </script>
 
 <?php
     $servername = "localhost";
@@ -56,8 +33,47 @@
     $sql = 'SELECT id,tijd FROM history';
     $q = $conn->query($sql);
     $q -> setFetchMode(PDO::FETCH_ASSOC);
-
+    
+    function get(){
+	$sql2 = "SELECT * FROM alarm";
+	$result2 = mysqli_query($conn,$sql2);
+	return $result2;
+    }
+  
+    function write($alarm){
+	if ($alarm == 1) {
+	    $sql3 = "INSERT INTO alarm (stat) VALUES (1)";
+	    mysqli_query($conn,$sql3);
+	} else {
+	    $sql4 = "INSERT INTO alarm (stat) VALUES (0)";
+	    mysqli_query($conn,$sql4);
+	}
+    }
 ?>
+    <script>
+	var alarm = <?php echo get();?>;
+	if (alarm == 1) {
+		document.getElementById("buttontext").innerHTML = "Thuis";
+		document.getElementById("thuis").style.backgroundColor = "green";
+	} else {
+		document.getElementById("buttontext").innerHTML = "Weg";
+		document.getElementById("thuis").style.backgroundColor = "red";
+	}
+
+	function button() {
+		if (alarm == "1") {
+			alarm = 0;
+			document.getElementById("buttontext").innerHTML = "Thuis";
+			document.getElementById("thuis").style.backgroundColor = "green";
+			<?php write(0);?>;
+		} else if (alarm == "0"){
+			alarm = 1;
+			document.getElementById("buttontext").innerHTML = "Weg";
+			document.getElementById("thuis").style.backgroundColor = "red";
+			<?php write(1);?>;
+		}
+	}
+    </script>
 <div>
 <table>
   <tr>
