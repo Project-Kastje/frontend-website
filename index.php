461424e9e18e7792bc945eb1b0d9c7be5059c2fd
JobVulkers <55084678+JobVulkers@users.noreply.github.com>
2019-09-13 11:58:09 +0200

    JAJA

--- index.php
+++ index.php
@@ -46,12 +46,10 @@
     $password = "geheim!";
     $db = "kastje";
     try {
-        echo("2");
         $conn = new PDO("mysql:host=$servername;dbname=kastje", $username, $password);
-        echo("3");
+
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected successfully";
     } catch(PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
     }
